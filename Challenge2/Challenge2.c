024284a94e933d0741687b50d952ebe069c449c7
Daniel <dwood253@uw.edu>
2017-04-18 10:40:14 -0700

    initial commit

---
+++ Challenge2/Challenge2.c
@@ -0,0 +1,15 @@
+#pragma config(Sensor, S1,     colorLeft,         sensorEV3_Color)
+#pragma config(Sensor, S3,     colorRight,         sensorEV3_Color)
+#pragma config(Sensor, S2,     sonar,         sensorEV3_Ultrasonic)
+#pragma config(Motor,  motorA,          leftWheel,     tmotorEV3_Large, PIDControl, encoder)
+#pragma config(Motor,  motorD,          rightWheel,    tmotorEV3_Large, PIDControl, encoder)
+
+
+
+
+task main()
+{
+
+
+
+}
