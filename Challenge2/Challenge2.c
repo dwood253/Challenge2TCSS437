8380230df0797ce0aaeb6baea16c291b92642072
Kyle Housley <kyle@bill4time.com>
2017-04-20 09:17:48 -0700

    imported random walk

--- Challenge2/Challenge2.c
+++ Challenge2/Challenge2.c
@@ -1,31 +1,56 @@
 #pragma config(Sensor, S1,     colorLeft,         sensorEV3_Color)
 #pragma config(Sensor, S3,     colorRight,         sensorEV3_Color)
 #pragma config(Sensor, S2,     sonar,         sensorEV3_Ultrasonic)
-#pragma config(Motor,  motorA,          leftWheel,     tmotorEV3_Large, PIDControl, encoder)
-#pragma config(Motor,  motorD,          rightWheel,    tmotorEV3_Large, PIDControl, encoder)
+#pragma config(Motor,  motorA,          leftMotor,     tmotorEV3_Large, PIDControl, encoder)
+#pragma config(Motor,  motorD,          rightMotor,    tmotorEV3_Large, PIDControl, encoder)
 
 
-
-void testSonar () {
-	int val = getDistanceValue(sonar);
-	writeDebugStreamLine("sonar val = %d", val);
+//this is for wondering right
+void randomRight() {
+	setMotorSpeed(leftMotor, 60);
+	setMotorSpeed(rightMotor, 50);
 }
 
-void testColor () {
-	setColorMode(colorLeft, colorTypeGrayscale_Reflected);
-	setColorMode(colorRight, colorTypeGrayscale_Reflected);
+//this is for wondering left
+void randomLeft() {
+	setMotorSpeed(leftMotor, 50);
+	setMotorSpeed(rightMotor, 60);
+}
 
-	int val = getColorGrayscale(colorLeft),
-	    val2 = getColorGrayscale(colorRight);
-	writeDebugStreamLine("color left is %d, and color right is %d", val, val2);
+//this is the random walking task.
+task randomWalk() {
+	//varibles that
+	long rTime = 0;
+	long turnProb = 0;
+	long rightProbability = 50;
+	long leftProbability = 50;
+
+
+//this loop will let robot walking repeatly but with random pattern.
+  while(true) {
+  	//generates number between 0 and 99.
+  	//starts with 50/50 chance of going left or right.
+  	//lower end of probability will always be for turning left.
+  	turnProb = rand() % 100;
+  	rTime = rand() % 900 + 300;
+
+	//random turning algorithm.
+  	if(turnProb < leftProbability) {
+  		randomLeft();
+  		rightProbability += 10;
+  		leftProbability -= 10;
+  	} else {
+  		randomRight();
+  		rightProbability -= 10;
+  		leftProbability += 10;
+  	}
+		wait1Msec(rTime);
+	}
 }
 
 task main()
 {
-	while (true) {
-		testSonar();
-		//testColor();
-	}
+	startTask(randomWalk);
 
-
+	while (true);
 }
