704b62bb23fdcd1268b8c3f6d73c4923bac943c2
Kyle Housley <kyle@bill4time.com>
2017-04-20 10:00:01 -0700

    imported random walk

--- Challenge2/Challenge2.c
+++ Challenge2/Challenge2.c
@@ -4,6 +4,12 @@
 #pragma config(Motor,  motorA,          leftMotor,     tmotorEV3_Large, PIDControl, encoder)
 #pragma config(Motor,  motorD,          rightMotor,    tmotorEV3_Large, PIDControl, encoder)
 
+#define COLOR_SENSOR_AVERAGE_BUFFER_LEN 10
+#define SONAR_SENSOR_AVERAGE_BUFFER_LEN 10
+
+float getMovingAvg (float lastAvg, float alpha, int length, int newestReading) {
+  return lastAvg + (alpha * (newestReading - lastAvg));
+}
 
 //this is for wondering right
 void randomRight() {
@@ -18,6 +24,7 @@ void randomLeft() {
 }
 
 //this is the random walking task.
+//Lowest priority 2
 task randomWalk() {
 	//varibles that
 	long rTime = 0;
@@ -48,8 +55,53 @@ task randomWalk() {
 	}
 }
 
+
+//Priority 1
+//Kill wander task to follow line
+//Black is approximately 10 +- 5
+//White is approximately 70 +- 10
+task lineFollow () {
+	stopTask(randomWalk);
+
+	//DO the right thing
+
+	startTask(randomWalk);
+}
+
+
+//Priority 0
+//Kill line detection to chase object and kill wander
+//3 feet is approximately 90 from the getDistance function
+task objectDetect () {
+	stopTask(lineFollow);
+	stopTask(randomWalk);
+
+	//DO the right thing
+
+	startTask(lineFollow);
+	startTask(randomWalk);
+}
+
+
+
+
 task main()
 {
+	//initialize left/right color sensor average
+	//init distance sensor average
+	int [COLOR_SENSOR_AVERAGE_BUFFER_LEN] colorAvg;
+	int [SONAR_SENSOR_AVERAGE_BUFFER_LEN] sonarAvg;
+	for (int i = 0; i < COLOR_SENSOR_AVERAGE_BUFFER_LEN; i++) {
+
+  }
+
+  for (i = 0; i < SONAR_SENSOR_AVERAGE_BUFFER_LEN; i++) {
+
+  }
+
+
+	startTask(objectDetect);
+	startTask(lineFollow);
 	startTask(randomWalk);
 
 	while (true);
